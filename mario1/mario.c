b870c8a05cad340a37170f80e78a22435a1ce56c
yshakk <107831515+yshakk@users.noreply.github.com>
2022-07-24 10:56:58 +0530

    Add files via upload

---
+++ mario1/mario.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    
+    int height = 0;
+    do
+    {
+        height = get_int("Height:");
+      
+        
+    }while((height <=0) || (height >=9));
+    /*
+    for(int k=0;k<height;k++)
+{
+    for(int i = 0; i <= k;i++ )
+    {
+        printf("#");
+    }
+    printf("\n");
+}*/
+
+
+for(int k=0;k<height;k++)
+{
+    for(int j=height;j>k+1;j--)
+    {
+        printf(" ");
+    }
+    
+  
+    
+   for(int i = 0; i <= k;i++ )
+    {
+        printf("#");
+    }
+    
+      printf("  ");
+    for(int j = 0; j <= k;j++ )
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+    
+}
